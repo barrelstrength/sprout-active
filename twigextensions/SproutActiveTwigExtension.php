0f7f58a88a4add5f40873a1fdc8d82ae572046a6
Patrick Pohler <ppohler@gmail.com>
2015-02-12 14:43:09 -0500

    Resolve issues w/ not setting CRAFT_SITE_URL and locale settings. This should return the proper url for comparison

--- twigextensions/SproutActiveTwigExtension.php
+++ twigextensions/SproutActiveTwigExtension.php
@@ -5,7 +5,7 @@ class SproutActiveTwigExtension extends \Twig_Extension
 {
   /**
    * Plugin Name
-   * 
+   *
    * @return string
    */
   public function getName()
@@ -15,7 +15,7 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Create our Twig Functions
-   * 
+   *
    * @return array
    */
   public function getFunctions()
@@ -30,11 +30,11 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Output the classname if conditions match
-   * 
+   *
    * @param  string  $string    Value from URL to use in test
    * @param  integer $segment   URL segment to test against
    * @param  string  $className Value of CSS class to return to template
-   * @return string OR null          
+   * @return string OR null
    */
   public function getActive($string = '', $segment = 1, $className = 'active')
   {
@@ -45,14 +45,14 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Output the classname and class parameter if conditionals match
-   * 
+   *
    * @param  string  $string    Value from URL to use in test
    * @param  integer $segment   URL segment to test against
    * @param  mixed  $className Value of CSS class to return to template
    * @return mixed OR null
    */
   public function getActiveClass($string = '', $segment = 1, $className = 'active')
-  { 
+  {
     $segment = $this->_processSegment($segment);
     $activeClassString = 'class="' . $className . '"';
 
@@ -61,7 +61,7 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Output the segment if conditions match
-   * 
+   *
    * @param  integer $segment URL segment to test for
    * @return string OR null   Value of URL segment if it exists
    */
@@ -72,7 +72,7 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Output the segment and class parameter if conditions match
-   * 
+   *
    * @param  integer $segment URL segment to test for
    * @return mixed OR null    Value of URL segment wrapped in class parameter if it exists
    */
@@ -86,21 +86,34 @@ class SproutActiveTwigExtension extends \Twig_Extension
 
   /**
    * Determine how to process the segment requested
-   * 
+   *
    * @param  mixed $segment Segment number or keyword
    * @return string         Segment, Path, or Full URL
    */
   private function _processSegment($segment)
-  { 
+  {
     switch ($segment) {
       case 'url':
-        return CRAFT_SITE_URL . craft()->request->url;
+        if(defined('CRAFT_SITE_URL')) {
+            return CRAFT_SITE_URL . craft()->request->url;
+        } else {
+            $localeId = craft()->getLocale()->id;
+            $localizedSiteUrl = craft()->config->getLocalized('siteUrl', $localeId);
+
+            //unless 'omitScriptNameInUrls' is explicitly set to 'true' then page.url will
+            //include index.php, we'll have to add it to the localizedSiteUrl
+            if(craft()->config->getLocalized('omitScriptNameInUrls', $localeId) === TRUE || strpos($localizedSiteUrl, 'index.php') !== FALSE) {
+                return rtrim($localizedSiteUrl, '/') . '/' . craft()->request->path;
+            } else {
+                return rtrim($localizedSiteUrl, '/') . '/index.php/' . craft()->request->path;
+            }
+        }
         break;
 
       case 'path':
         return craft()->request->path;
         break;
-      
+
       default:
         return craft()->request->getSegment($segment);
         break;
