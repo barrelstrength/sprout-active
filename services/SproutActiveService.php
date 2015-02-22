3bab6f3cf988e3856b3bdc75a6c0b134d20b3331
Ben Parizek <ben@barrelstrengthdesign.com>
2015-02-22 02:32:50 -0800

    Fix logic with index.php in URLs

--- services/SproutActiveService.php
+++ services/SproutActiveService.php
@@ -12,14 +12,14 @@ class SproutActiveService extends BaseApplicationComponent
 	 */
 	public function match($string, $segment)
 	{
-	  // Get the slug version of the segment
-	  $segment = $this->processSegment($segment);
+		// Get the slug version of the segment
+		$matchString = $this->processSegment($segment);
 
-	  // Convert our input into an array
-	  $matchOptions = explode('|', $string);
+		// Convert our input into an array
+		$matchOptions = explode('|', $string);
 
-	  // Return true if the segment matches any of our test values
-	  return in_array($segment, $matchOptions);
+		// Return true if the segment matches any of our test values
+		return in_array($matchString, $matchOptions);
 	}
 
 	/**
@@ -30,18 +30,54 @@ class SproutActiveService extends BaseApplicationComponent
 	 */
 	public function processSegment($segment)
 	{ 
-	  switch ($segment) {
-	    case 'url':
-	      return CRAFT_SITE_URL . craft()->request->url;
-	      break;
+		switch ($segment)
+		{
+			case 'url':
+				return $this->_getUrl($segment);
+				break;
 
-	    case 'path':
-	      return craft()->request->path;
-	      break;
-	    
-	    default:
-	      return craft()->request->getSegment($segment);
-	      break;
-	  }
-	}	
+			case 'path':
+				return craft()->request->path;
+				break;
+			
+			default:
+				return craft()->request->getSegment($segment);
+				break;
+		}
+	}
+
+	private function _getUrl()
+	{	
+		if (defined('CRAFT_SITE_URL')) 
+		{	
+			return CRAFT_SITE_URL . craft()->request->url;
+		}
+		else
+		{	
+			$localeId = craft()->getLocale()->id;
+
+			$localizedSiteUrl = craft()->config->getLocalized('siteUrl', $localeId);
+
+			if (!$localizedSiteUrl) 
+			{
+				$localizedSiteUrl = craft()->getSiteUrl();
+			}
+
+			$localizedSiteUrl = rtrim($localizedSiteUrl, '/');
+
+			// Unless 'omitScriptNameInUrls' is explicitly set to 'true' then page.url will
+			// include index.php, we'll have to add it to the localizedSiteUrl
+			$omitScriptNameInUrls = craft()->config->getLocalized('omitScriptNameInUrls', $localeId) === TRUE;
+			$noIndexInUrls = strpos($localizedSiteUrl, 'index.php') !== TRUE;
+
+			if($omitScriptNameInUrls || $noIndexInUrls) 
+			{	
+				return $localizedSiteUrl . '/' . craft()->request->path;
+			}
+			else
+			{	
+				return $localizedSiteUrl . '/index.php/' . craft()->request->path;
+			}
+		}
+	}
 }
