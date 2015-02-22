ff2317e99d521e32722f859d08b262e84cf8d744
Ben Parizek <ben@barrelstrengthdesign.com>
2015-02-21 23:59:56 -0800

    Update to use tabs

--- SproutActivePlugin.php
+++ SproutActivePlugin.php
@@ -3,36 +3,35 @@ namespace Craft;
 
 class SproutActivePlugin extends BasePlugin
 {
-    public function getName()
-    {
-        return Craft::t('Sprout Active');
-    }
+	public function getName()
+	{
+		return Craft::t('Sprout Active');
+	}
 
-    public function getVersion()
-    {
-        return '0.7.0';
-    }
+	public function getVersion()
+	{
+		return '0.7.0';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Barrel Strength Design';
-    }
+	public function getDeveloper()
+	{
+		return 'Barrel Strength Design';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'http://barrelstrengthdesign.com';
-    }
+	public function getDeveloperUrl()
+	{
+		return 'http://barrelstrengthdesign.com';
+	}
 
-    public function hasCpSection()
-    {
-        return false;
-    }
+	public function hasCpSection()
+	{
+		return false;
+	}
 
-    public function addTwigExtension()
-    {
-        Craft::import('plugins.sproutactive.twigextensions.SproutActiveTwigExtension');
-
-        return new SproutActiveTwigExtension();
-    }
-
+	public function addTwigExtension()
+	{
+		Craft::import('plugins.sproutactive.twigextensions.SproutActiveTwigExtension');
+		
+		return new SproutActiveTwigExtension();
+	}
 }
