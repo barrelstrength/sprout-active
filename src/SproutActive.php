78b00a70c19b7df976d1e8a85e11020310ef5615
Ben Parizek <ben@barrelstrengthdesign.com>
2018-05-11 17:46:19 -0400

    Move schema and component definitions to Plugin class

--- src/SproutActive.php
+++ src/SproutActive.php
@@ -16,9 +16,21 @@ class SproutActive extends Plugin
      */
     public static $app;
 
+    /**
+     * @var string
+     */
+    public $schemaVersion = '2.0.0';
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     */
     public function init()
     {
         parent::init();
+
+        $this->setComponents([
+            'app' => App::class
+        ]);
 
         self::$app = $this->get('app');
 
