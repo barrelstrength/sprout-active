9a27ce769539063ea069fed7da874583047c83eb
Ben Parizek <ben.parizek@gmail.com>
2020-01-22 21:17:54 -0500

    Adds copyright headers

--- src/services/Utilities.php
+++ src/services/Utilities.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://sprout.barrelstrengthdesign.com
+ * @copyright Copyright (c) Barrel Strength Design LLC
+ * @license MIT
+ */
 
 namespace barrelstrength\sproutactive\services;
 
